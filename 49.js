7422cfaa6d983460356dc707c11c068c6f9600bc
seven.zhong <seven.zhong@tec-do.com>
2026-01-08 14:31:50 +0800

    260108

---
+++ 49.js
@@ -0,0 +1,21 @@
+
+
+var groupAnagrams = function(strs) {
+    const map  = new Map();
+    for(let i=0;i<strs.length;i++){
+        let str = strs[i];
+        let key = str.split('').sort().join('');
+        if(map.has(key)){
+            map.get(key).push(str);
+        }else{
+            map.set(key,[str]);
+        }
+    }
+
+    return Array.from(map.values());
+};
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+
+console.log(groupAnagrams(strs));
