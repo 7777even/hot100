713157f080aa8ad5d28ef20c96a5e3d5f9f08563
7777even <3289228667@qq.com>
2026-01-08 15:58:50 +0800

    feat:128. 最长连续序列

--- 128.js
+++ 128.js
@@ -1,30 +1,23 @@
-var longestConsecutive = function (nums) {
-    if (nums.length === 0) return 0;
-
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if(nums.length === 0) return 0;
     let set = new Set(nums);
-
     let max = -1;
-
-    for (let temp of set) {
-        if (set.has(temp - 1) === false) {
+    for(let temp of set){
+        if(set.has(temp-1) === false){
             let start;
             start = temp;
-            while (set.has(temp)) {
+            while(set.has(temp)){
                 temp++;
             }
             let end;
-            end = temp;
-            max = Math.max(max, end - start);
-        }
+            end = temp
+            max = Math.max(max,end-start);
     }
-    return max;
 }
 
-<<<<<<< HEAD
-const nums = [100, 4, 200, 1, 3, 2];
-console.log(longestConsecutive(nums));
-=======
-const nums = [100,4,200,1,3,2];
-console.log(longestConsecutive(nums));
->>>>>>> 9f4179d72d3e4e17a3a187849f1e665389695a5c
+return max;
+}
