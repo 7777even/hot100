a9cc8f9940b0fc4b38e064f8920c94c5916f0d8a
7777even <3289228667@qq.com>
2026-01-08 15:51:25 +0800

    feat:42. 接雨水

--- 128.js
+++ 128.js
@@ -1,25 +1,25 @@
-var longestConsecutive = function(nums){
-    if(nums.length===0) return 0;
+var longestConsecutive = function (nums) {
+    if (nums.length === 0) return 0;
 
 
     let set = new Set(nums);
 
     let max = -1;
 
-    for(let temp of set){
-        if(set.has(temp-1)===false){
+    for (let temp of set) {
+        if (set.has(temp - 1) === false) {
             let start;
             start = temp;
-            while(set.has(temp)){
+            while (set.has(temp)) {
                 temp++;
             }
             let end;
             end = temp;
-            max = Math.max(max,end-start);
+            max = Math.max(max, end - start);
         }
     }
     return max;
 }
 
-const nums = [100,4,200,1,3,2];
+const nums = [100, 4, 200, 1, 3, 2];
 console.log(longestConsecutive(nums));
--- 49.js
+++ 49.js
@@ -1,14 +1,12 @@
-
-
-var groupAnagrams = function(strs) {
-    const map  = new Map();
-    for(let i=0;i<strs.length;i++){
+var groupAnagrams = function (strs) {
+    const map = new Map();
+    for (let i = 0; i < strs.length; i++) {
         let str = strs[i];
         let key = str.split('').sort().join('');
-        if(map.has(key)){
+        if (map.has(key)) {
             map.get(key).push(str);
-        }else{
-            map.set(key,[str]);
+        } else {
+            map.set(key, [str]);
         }
     }
 
