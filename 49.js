2de40ef73fd5125c588bce702f30c9e552ca48eb
7777even <3289228667@qq.com>
2026-01-08 15:55:54 +0800

    feat:49. 字母异位词分组

--- 49.js
+++ 49.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-var groupAnagrams = function (strs) {
-    const map = new Map();
-    for (let i = 0; i < strs.length; i++) {
-=======
 var groupAnagrams = function(strs) {
     const map  = new Map();
     for(let i=0;i<strs.length;i++){
->>>>>>> 9f4179d72d3e4e17a3a187849f1e665389695a5c
         let str = strs[i];
         let key = str.split('').sort().join('');
-        if (map.has(key)) {
+        if(map.has(key)){
             map.get(key).push(str);
-        } else {
-            map.set(key, [str]);
+        }else{
+            map.set(key,[str]);
         }
     }
 
     return Array.from(map.values());
 };
-
-const strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-
-
-console.log(groupAnagrams(strs));
