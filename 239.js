66731f2de3b8b68fba0c291fe60dbeac4545dea9
7777even <3289228667@qq.com>
2026-01-16 17:27:19 +0800

    feat:239. 滑动窗口最大值

---
+++ 239.js
@@ -0,0 +1,29 @@
+var maxSlidingWindow = function (nums, k) {
+
+    const deque = [];
+    const res = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        while (deque.length && nums[i] >= nums[deque[deque.length - 1]]) {
+
+            deque.pop();
+
+        }
+
+        deque.push(i);
+
+
+        if (deque[0] <= i - k) {
+
+            deque.shift()
+
+        }
+
+        if (i >= k - 1) {
+            res.push(nums[deque[0]]);
+        }
+
+    }
+    return res;
+};
